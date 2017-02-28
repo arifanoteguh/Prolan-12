4e5a722424a1679aea8fceea4cb80fe6c4ffd288
arifanoteguh <arifanoteguh>
2017-02-28 16:32:59 +0700

    Mahasiswa Java

---
+++ Minggu2/Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa{
+	public static void main(String argv[]){
+		System.out.println("Nama Lengkap : Arifano Teguh Wicaksono");
+		System.out.println("NIM          : 10114134");
+		System.out.println("Alamat       : Simpang Dago");
+	}
+}
