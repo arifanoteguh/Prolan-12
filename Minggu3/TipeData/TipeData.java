c3dd6efdcbf2a2d90cb99fdafb0b998ad34f4ada
arifanoteguh <arifanoteguh>
2017-03-08 16:24:07 +0700

    TipeData + Modifikasi

---
+++ Minggu3/TipeData/TipeData.java
@@ -0,0 +1,52 @@
+public class TipeData{
+	public static void main(String[] args){
+		//Tipe data objek
+		String nama = "Arifano Teguh Wicaksono";
+		System.out.println(nama.length());
+
+		//Tipe data byte
+		byte maxByte = 127;
+		byte minByte = -128;
+		byte defaultByte = 0;
+		System.out.println(minByte);
+		
+		//Tipe data short
+		short maxShort = 32767;
+		short minShort = -32768;
+		short defaultShort = 0;
+		System.out.println(defaultShort);
+		
+		//Tipe data int
+		int maxInt = 2147483647;
+		int minInt = -2147483648;
+		int defaultInt = 0;
+		System.out.println(defaultInt);
+		
+		//Tipe data long
+		long maxLong = 9223372036854775807L;
+		long minLong = -9223372036854775808L;
+		long defaultLong = 0L;
+		System.out.println(defaultLong);
+		
+		//Tipe data float
+		float maxFloat = 3.4028234799999999999999999999999999999999999999f;
+		float minFloat = -3.4028234999999999999999999999999999999999999999f;
+		float defaultFloat = 0;
+		System.out.println(minFloat);
+		
+		//Tipe data double
+		double randomDouble = 1.79763134862315702d;
+		double randomDoublel = -1.797693134862315602d;
+		double defaultDouble = 0;
+		System.out.println(randomDouble);
+		System.out.println(randomDoublel);
+
+		//Tipe data boolean
+		boolean menikah = false;
+		System.out.println(menikah);
+
+		//Tipe data char
+		char A = 'A';
+		System.out.println(A);
+	}
+}
