e1950465e66190b4f46d7f8146188bf95d813971
arifanoteguh <arifanoteguh>
2017-04-04 20:47:47 +0700

    Override

---
+++ Minggu6/override/Manusia.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Manusia{
+	Scanner input=new Scanner(System.in);
+	String nama;
+	byte umur;
+	char jk;
+	int hari;
+	long jam;
+	float detik;
+
+	public Manusia(){
+		System.out.print("Nama Lengkap : ");
+		this.nama=input.nextLine();
+	}
+	void berumur(){
+		System.out.print("Umur : ");
+		umur=input.nextByte();
+	}
+	void jenisKelamin(){
+		System.out.print("Jenis Kelamin? [L/P] : ");
+		jk=input.next().charAt(0);
+	}
+	void hidupDalamHari(){
+		hari=umur*365;
+		System.out.println("Umur dalam hari : "+hari);
+	}
+	void hidupDalamJam(){
+		jam=hari*24;
+		System.out.println("Umur dalam jam : "+jam);
+	}
+	void hidupDalamDetik(){
+		detik=jam*60;
+		System.out.println("Umur dalam jam : "+detik);
+	}
+}
